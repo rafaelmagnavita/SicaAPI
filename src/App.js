0f7d98a54a19ca2919eb4888fc948de554b9551d
Rafael Magnavita <rafaelmagnavita@hotmail.com>
2023-08-15 17:15:53 -0300

    Consumindo api sicra com frontEnd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import api from "./Api.js"; // Import the API service
 
-function App() {
+export default function App() {
+  const [solicitacao, setUser] = useState();
+  const [idValue, setIdValue] = useState();
+
+  const fetchData = () => {
+    const requestData = {
+      id: idValue
+    };
+
+    api
+      .post("/Solicitacao", requestData)
+      .then((response) => {
+        console.log("API Response:", response.data);
+        setUser(response.data);
+      })
+      .catch((err) => {
+        console.error("Oops! An error occurred: " + err);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor="idInput">Código Solicitação:</label>
+        <input
+          id="idInput"
+          type="number"
+          value={idValue}
+          onChange={(e) => setIdValue(Number(e.target.value))}
+        />
+        <button onClick={fetchData}>Fetch Data</button>
+      </div>
+      {solicitacao !== null ? (
+        <div>
+        <p><b>Usuario:</b> {solicitacao?.Usuario}</p>
+        <p><b>Data Início SLA:</b> {solicitacao?.DataInicioSla}</p>
+        <p><b>Data de Criação:</b> {solicitacao?.DataCriacao}</p>
+        <p><b>Data Alteração:</b> {solicitacao?.DataStatus}</p>
+        <p><b>Id Vinculo:</b> {solicitacao?.ColaboradorEmpresaId}</p>
+
+        <p><b>Status:</b> {solicitacao?.Status}</p>
+        </div>
+      ) : (
+        <p>Nenhuma Solicitação Encontrada</p>
+      )}
     </div>
   );
 }
-
-export default App;
