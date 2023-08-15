4b2d435702e60212c977a2b0a91c9ffdc3d77a12
Rafael Magnavita <rafaelmagnavita@hotmail.com>
2023-08-15 18:01:02 -0300

    Retornando lista de solicitações por empresa

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import api from "./Api.js"; // Import the API service
 
 export default function App() {
-  const [solicitacao, setUser] = useState();
+  const [solicitacoes, setUser] = useState();
   const [idValue, setIdValue] = useState();
 
   const fetchData = () => {
     const requestData = {
-      id: idValue
+      id: idValue,
     };
 
     api
-      .post("/Solicitacao", requestData)
+      .post("/SolicitacoesWeb", requestData)
       .then((response) => {
-        console.log("API Response:", response.data);
+        console.log(response.data);
         setUser(response.data);
       })
       .catch((err) => {
@@ -22,9 +22,9 @@ export default function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App" style={{ marginLeft: "20px", marginTop: "20px" }}>
       <div>
-        <label htmlFor="idInput">Código Solicitação:</label>
+        <label htmlFor="idInput">Código Empresa:</label>
         <input
           id="idInput"
           type="number"
@@ -33,15 +33,36 @@ export default function App() {
         />
         <button onClick={fetchData}>Fetch Data</button>
       </div>
-      {solicitacao !== null ? (
+      {solicitacoes != null ? (
         <div>
-        <p><b>Usuario:</b> {solicitacao?.Usuario}</p>
-        <p><b>Data Início SLA:</b> {solicitacao?.DataInicioSla}</p>
-        <p><b>Data de Criação:</b> {solicitacao?.DataCriacao}</p>
-        <p><b>Data Alteração:</b> {solicitacao?.DataStatus}</p>
-        <p><b>Id Vinculo:</b> {solicitacao?.ColaboradorEmpresaId}</p>
-
-        <p><b>Status:</b> {solicitacao?.Status}</p>
+          <h2>Solicitações:</h2>
+          <hr></hr>
+          {solicitacoes.map((solicitacaoItem, index) => (
+            <div key={index}>
+              <p>
+                <b>Id:</b> {solicitacaoItem.Id}
+              </p>
+              <p>
+                <b>Usuario:</b> {solicitacaoItem.Usuario}
+              </p>
+              <p>
+                <b>Data Início SLA:</b> {solicitacaoItem.DataInicioSla}
+              </p>
+              <p>
+                <b>Data de Criação:</b> {solicitacaoItem.DataCriacao}
+              </p>
+              <p>
+                <b>Data Alteração:</b> {solicitacaoItem.DataStatus}
+              </p>
+              <p>
+                <b>Id Vinculo:</b> {solicitacaoItem.ColaboradorEmpresaId}
+              </p>
+              <p>
+                <b>Status:</b> {solicitacaoItem.NomeStatus}
+              </p>
+              <hr></hr>
+            </div>
+          ))}
         </div>
       ) : (
         <p>Nenhuma Solicitação Encontrada</p>
